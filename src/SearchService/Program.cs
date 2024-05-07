5a519fb5fe7ba845bdc2ab60dc3949c1841f68db
Adir Noyman <ofahol2007@hotmail.com>
2024-05-07 23:13:09 +0300

    .

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,7 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,6 +15,16 @@ var app = builder.Build();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Create the MongoDB connection
+await DB.InitAsync("SearchDB", MongoClientSettings.FromConnectionString(builder.Configuration.GetConnectionString("MongoDBConnection")));
+
+// Create the MongoDB Indexes
+await DB.Index<Item>()
+.Key(x => x.Make, KeyType.Text)
+.Key(x => x.Model, KeyType.Text)
+.Key(x => x.Color, KeyType.Text)
+.CreateAsync();
 
 
 
